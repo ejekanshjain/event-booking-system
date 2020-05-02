df73131c81b30b55ef4100ab582c8bc897254581
ejekanshjain <ejekanshjain@gmail.com>
2020-05-02 16:19:44 +0530

    setup basic graphql api server

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # event-booking-system
+
+## Install dependencies
+## npm i
+
+## Start Server
+## npm start
+
+## Start Server with nodemon
+## npm run dev
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const express = require('express')
+const expressGraphQL = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const port = process.env.PORT
 
@@ -7,8 +9,31 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!')
-})
+app.use('/graphql', expressGraphQL({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Event 1', 'Event 2']
+        },
+
+        createEvent: args => {
+            return args.name
+        }
+    },
+    graphiql: process.env.NODE_ENV !== 'production' ? true : false
+}))
 
 app.listen(port, () => console.log(`${process.env.NODE_ENV !== 'production' ? 'Development' : 'Production'} Server started on port ${port}...`))
