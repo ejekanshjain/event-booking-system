6101185f3d07481ea9721d73898a43e6ab036193
ejekanshjain <ejekanshjain@gmail.com>
2020-05-02 21:31:23 +0530

    added mongoose package and setup db and basic event model

---
+++ db/MongoDB.js
@@ -0,0 +1,15 @@
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+const mongoose = require('mongoose')
+
+mongoose
+    .connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => {
+        console.log(err)
+        process.exit(1)
+    })
+
+module.exports = mongoose.connection
